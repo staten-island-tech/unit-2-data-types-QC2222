0622184f8419a8e62e817d31485a5f4c13d9065b
QC2222 <quintonc12@nycstudents.net>
2026-02-11 09:57:55 -0500

    1

--- every factor.py
+++ every factor.py
@@ -1,17 +1,6 @@
-x = input("Number")
-x = int(x)
+x = int(input("Number"))
 y = int(1)
-if ((x % 2) == 0):
-    for i in range(x + 1):
-        if ((x % y) > 0):
-            y = float(y + 1)
-        elif  ((x % y) == 0):
-            print(y)
-            y = float(y + 1)
-else:
-    for i in range(x):
-        if ((x % y) > 0):
-            y = float(y + 2)
-        elif  ((x % y) == 0):
-            print(y)
-            y = float(y + 2)
+for i in range(x + 1):
+    if ((x % y) == 0):
+        print(y)
+    y = (y + 1)
